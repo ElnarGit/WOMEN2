55c7d9c93d5ea28e6af96452c807e599ae19b480
Elnar Saparov <elnar_saparov@mail.ru>
2022-02-01 14:37:30 +0600

    add slug

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7_ocpo7m1_gu4%an%2t#ibj(jyvc8p8u1k^e#o7-l)$tz)z05o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
