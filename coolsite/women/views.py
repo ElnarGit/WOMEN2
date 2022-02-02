62f66450ade850d5911771cae22a4cad56edebd0
Elnar Saparov <elnar_saparov@mail.ru>
2022-02-02 14:56:47 +0600

    Работа с шаблонами

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,13 +1,21 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 
+from .models import *
+
+menu = ["О сайте","Добавить статью","Обратная связь","Войти"]
+
 
 def index(request):
-    return HttpResponse("Страница приложения women")
+    posts = Women.objects.all()
+    return render(request,'women/index.html', {'posts':posts, 'menu':menu, 'title': 'Главная страница'})
+
+def about(request):
+    return render(request,'women/about.html',{'menu':menu, 'title': 'О сайте'})
 
 def categories(request,catid):
-    #if(request.POST):
-        #print(request.POST)
+    if(request.POST):
+        print(request.POST)
     return HttpResponse(f"<h1>Статьи по категория</h1><p>{catid}</p>")
 
 
